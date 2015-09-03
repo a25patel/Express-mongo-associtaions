02869fbe696acf6f25ac52f1626c649b097fc766
a25patel <aanaletap2510@gmail.com>
2015-09-03 15:35:07 -0600

    working people.address

--- routes/index.js
+++ routes/index.js
@@ -7,18 +7,37 @@ var addresses = db.get('addresses')
 var information = require('../lib/mongo.js')
 
 /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   people.find({}).then(function(documents){
-//     var master = {};
-//     master.people = documents;
-//     return master
-//   }).then(function(master){
-//     return addresses.find({_id: documents.addressId}).then(function(address){
-//       master.address = address;
-//       return master
-//     })
-//       res.render('index' ,master);
-//     })
-//   });
+router.get('/', function(req, res, next) {
+  people.find({}).then(function(documents){
+    var master = {};
+    master.people = documents;
+    return master
+  }).then(function(master){
+    var array = master.people.map(function(person){
+      return person.addressId
+    })
+    console.log('jninuubu');
+    return addresses.find({_id:{$in: array}}).then(function(address){
+      master.people = master.people.map(function(person){
+        person.address = findMatchingAddress(person.addressId, address)
+        return person
+      })
+      return master
+    })
+  }).then(function(master){
+    console.log(master);
+    res.render('index' ,master);
+  })
+});
 
+
+
+function findMatchingAddress(addressId, addresses){
+  for (var i = 0; i < addresses.length; i++) {
+    if(addressId.toString() === addresses[i]._id.toString()){
+      return addresses[i];
+    }
+  }
+
+}
 module.exports = router;
